3230aa4cec80714dc5f5a891a2fa348d65e2d5d8
icechen1219 <sagachen@aliyun.com>
2018-11-08 23:02:46 +0800

    修改TargetModel类，为其所有方法添加执行时间计算。

--- src/main/java/com/cqgcxy/casestudy/case1/TargetModel.java
+++ src/main/java/com/cqgcxy/casestudy/case1/TargetModel.java
@@ -7,29 +7,44 @@ package com.cqgcxy.casestudy.case1;
 public class TargetModel {
 
     public void test1(){
+        long beginTime = System.currentTimeMillis();
+
         try {
             Thread.sleep(1500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("test1 method executed finish...");
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("test1 method used: "+(endTime-beginTime)+" milliseconds");
     }
 
     public void test2(){
+        long beginTime = System.currentTimeMillis();
+
         try {
             Thread.sleep(2500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("test2 method executed finish...");
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("test2 method used: "+(endTime-beginTime)+" milliseconds");
     }
 
     public void test3(){
+        long beginTime = System.currentTimeMillis();
+
         try {
             Thread.sleep(3500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("test3 method executed finish...");
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("test3 method used: "+(endTime-beginTime)+" milliseconds");
     }
 }
